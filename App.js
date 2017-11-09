27a14cf1289f594a9293df0d974d1a43537cbac8
Neil Haggerty <neil.haggerty@mergermarket.com>
2017-11-09 13:37:15 +0000

    Finished 57. Copying Reusable Components

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import {
   Text,
   View
 } from 'react-native';
+import { Header } from './src/components/common';
 
 export default class App extends Component {
   render() {
     return (
       <View>
+        <Header headerText="Authentication" />
         <Text>An App!</Text>
       </View>
     );
