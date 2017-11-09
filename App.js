5280cc5b627ffc046a42f62b6feb5f4041186db5
Neil Haggerty <neil.haggerty@mergermarket.com>
2017-11-09 13:43:13 +0000

    Finished 59. Firebase Client Setup

--- App.js
+++ App.js
@@ -3,9 +3,23 @@ import {
   Text,
   View
 } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './src/components/common';
 
+
 export default class App extends Component {
+  componentWillMount(){
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAGQ12ra2fRDXzfNbFPAGQ0vAfr-M2e9MA',
+      authDomain: 'authentication-19479.firebaseapp.com',
+      databaseURL: 'https://authentication-19479.firebaseio.com',
+      projectId: 'authentication-19479',
+      storageBucket: 'authentication-19479.appspot.com',
+      messagingSenderId: '760348568889'
+    });
+  }
+
   render() {
     return (
       <View>
